b981a3757ec731137fba9bea7f97ba5806013dd9
Zeb <zceboys@gmail.com>
2020-11-22 20:14:56 +0000

    setup.py name fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='easydb',
+    name='easysql',
     version='1.0.0',
     author='TeamNightSky',
     description='Python SQL query generator',
